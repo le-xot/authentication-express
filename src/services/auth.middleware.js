85cd80c3e91985c886db084cac3449416d2948ef
le-xot <lexot.dev@gmail.com>
2023-07-25 17:09:20 +0500

    Update auth.middleware.js

--- src/services/auth.middleware.js
+++ src/services/auth.middleware.js
@@ -1,7 +1,10 @@
 const jwt = require("jsonwebtoken");
+const { client } = require("./redis.service");
+const { generationAccessToken } = require("./generationAccessToken");
 
 function authMiddleware(req, res, next) {
   const accessToken = req.cookies.accessToken;
+  const refreshToken = req.cookies.refreshToken;
 
   if (!accessToken) {
     return res.redirect("/login");
@@ -9,8 +12,19 @@ function authMiddleware(req, res, next) {
 
   try {
     const secret = process.env.SECRET_TOKEN_ACCESS;
-    const decoded = jwt.verify(accessToken, secret);
-    next();
+    if (jwt.verify(accessToken, secret)) {
+      next();
+    } else {
+      client.get(refreshToken, (err, reply) => {
+        if (err || !reply) {
+          return res.redirect("/login");
+        }
+        const user = JSON.parse(reply);
+        const newAccessToken = generationAccessToken(user);
+        res.cookie("accessToken", newAccessToken);
+        next();
+      });
+    }
   } catch (error) {
     return res.redirect("/login");
   }
